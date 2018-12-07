06ff480a0bd5baa5af514076ece4ee527f66ad12
sunwei <1580440730@qq.com>
2018-12-07 15:07:15 +0800

    测试分支3

--- myapplication/src/main/java/node/com/myapplication/MainActivity.java
+++ myapplication/src/main/java/node/com/myapplication/MainActivity.java
@@ -12,41 +12,11 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     Button btnOnclick;
-    TextView tvContent;
-    DataViewModul dataViewModul;
-    int pos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dataViewModul = new ViewModelProvider.NewInstanceFactory().create(DataViewModul.class);
-
-        btnOnclick = findViewById(R.id.btnOnclick);
-        tvContent = findViewById(R.id.tvContent);
-
-
-        Observer<String> observer = new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                tvContent.setText(s);
-            }
-        };
-        dataViewModul.getGetName().observe(this, observer);
-
-        btnOnclick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pos++;
-                dataViewModul.getGetName().postValue(pos + "");
-            }
-        });
-
-
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 }
